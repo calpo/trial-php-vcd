e695f6941c32959f99d0cf94c9be9e0dc610759d
Calpo <calpo22@gmail.com>
2015-10-25 23:54:49 +0900

    add example

--- example.php
+++ example.php
@@ -5,6 +5,8 @@ require 'vendor/autoload.php';
 \VCR\VCR::turnOn();
 \VCR\VCR::insertCassette('example.yml');
 
-$json = file_get_contents('https://randomuser.me/api/');
-$data = json_decode($json, true);
-print_r($data['results'][0]);
+
+$result = file_get_contents(
+    'http://oshiete.goo.ne.jp/api/category'
+);
+print_r(json_decode($result, true));
