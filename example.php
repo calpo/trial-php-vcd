ffa8884676f1f10d6127d01bc413a88fb7a92a29
Calpo <calpo22@gmail.com>
2015-10-25 15:09:09 +0900

    add example

--- example.php
+++ example.php
@@ -1,7 +1,10 @@
 <?php
 require 'vendor/autoload.php';
+
+\VCR\VCR::configure()->setCassettePath('data');
 \VCR\VCR::turnOn();
+\VCR\VCR::insertCassette('example.yml');
 
-$result = file_get_contents('http://example.com');
-
-
+$json = file_get_contents('https://randomuser.me/api/');
+$data = json_decode($json, true);
+print_r($data['results'][0]);
